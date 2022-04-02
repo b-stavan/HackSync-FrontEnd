27c4e97a52b5af9d2b8690e88ca1e253d779c38d
Kavin Mehta <12kkmehta@gmail.com>
2022-04-02 23:57:07 +0530

    HomeScreen with switch to teammate modal

--- app/navigation/Routes.js
+++ app/navigation/Routes.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import MainStack from './MainStack';
-import IntroScreen from '../modules/IntroScreen/IntroScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainStack from "./MainStack";
+import IntroScreen from "../modules/IntroScreen/IntroScreen";
 const Stack = createNativeStackNavigator();
 
 const Routes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         {MainStack(Stack)}
       </Stack.Navigator>
     </NavigationContainer>
--- app/themes/Colors.js
+++ app/themes/Colors.js
@@ -1,7 +1,10 @@
 const Colors = {
   navyBlue: "rgba(26,26,64,1)",
+  lightNavyBlue: "rgba(rgb(0, 49, 110))",
   green: "rgba(149,205,65,1)",
-  white: "rgba(239,255,253,1)",
+  white: "rgba(255, 255, 255,1)",
+  transparentBlue: "rgba(26,26,64,0.9)",
+  transparentBlack: "rgba(0, 0, 0, 0.6)",
 };
 
 export default Colors;
