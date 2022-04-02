d6268a5f53c0ab568fe8db961a64ecee0e28e79f
Rutanshi <khambhatiyarutanshi248@gmail.com>
2022-04-02 12:00:20 +0530

    set up App navigation

---
+++ app/navigation/TabRoutes.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import navigationStrings from '../constants/navigationStrings';
+import {HomeScreen, Notification, EditProfile} from '../modules/index'
+const Tab = createBottomTabNavigator();
+const TabRoutes = () => {
+    return (
+        <Tab.Navigator screenOptions={{headerShown:false}}>
+          <Tab.Screen
+            name={navigationStrings.HOME_SCREEN}
+            component={HomeScreen}
+          />
+          <Tab.Screen
+            name={navigationStrings.NOTIFICATION}
+            component={Notification}
+          />
+          <Tab.Screen
+            name={navigationStrings.EDIT_PROFILE}
+            component={EditProfile}
+          />
+        </Tab.Navigator>
+      );
+}
+export default TabRoutes;
