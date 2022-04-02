b909a7753c5a32074a784edf11af65075a8846a3
Yogesh Late <yogesh.l@simformsolutions.com>
2022-04-02 19:53:57 +0530

    feat: bud fixed

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import React, { useEffect } from "react";
 import { SafeAreaView, Text } from "react-native";
 import SplashScreen from "react-native-splash-screen";
+import Routes from "./app/navigation/Routes";
+
 const App = () => {
   useEffect(() => {
     SplashScreen.hide();
   }, []);
   return (
-    <SafeAreaView>
-      <Text>App</Text>
+    <SafeAreaView style={{flex: 1}}>
+      <Routes/>
     </SafeAreaView>
   );
 };
--- app/navigation/MainStack.js
+++ app/navigation/MainStack.js
@@ -6,7 +6,7 @@ import TabRoutes from './TabRoutes';
 const MainStack = Stack => {
   return (
     <>
-          <Stack.Screen name='intro' component={IntroScreen}/>
+      <Stack.Screen name='intro' component={IntroScreen}/>
       <Stack.Screen name={navigationStrings.TABS} component={TabRoutes} />
     </>
 
