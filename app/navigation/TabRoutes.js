1e7ea969d48b7cbc0124775c8e7878605e1169ec
Het Ambani <het.a@simformsolutions.com>
2022-04-03 11:25:52 +0530

    wip: navigantion in progress.

--- app/navigation/TabRoutes.js
+++ app/navigation/TabRoutes.js
@@ -1,53 +1,74 @@
-
-import React from 'react';
-import {Image} from 'react-native'
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import navigationStrings from '../constants/navigationStrings';
-import {HomeScreen, Notification, EditProfile} from '../modules/index'
-import icons from '../assets/icons'
-import { Colors } from '../themes';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { Image } from "react-native";
+import icons from "../assets/icons";
+import navigationStrings from "../constants/navigationStrings";
+import { EditProfile, HomeScreen, Notification } from "../modules/index";
+import { Colors, moderateScale } from "../themes";
 
 const Tab = createBottomTabNavigator();
 
-const TabRoutes = () => {
-
-    return (
-        <Tab.Navigator screenOptions={{headerShown:false,tabBarShowLabel: false,}}>
-          <Tab.Screen
-            name={navigationStrings.HOME_SCREEN}
-            component={HomeScreen}
-            options={{
-              tabBarIcon: ({focused}) =>{
-                return (
-                  <Image style={{tintColor: focused ? Colors.navyBlue : Colors.grey,width:40,height:40}} source={icons.home}/>
-                )
-              }
-            }}
-          />
-          <Tab.Screen
-            name={navigationStrings.NOTIFICATION}
-            component={Notification}
-            options={{
-              tabBarIcon: ({focused}) =>{
-                return (
-                  <Image style={{tintColor: focused ? Colors.navyBlue : Colors.grey,width:40,height:40}} source={icons.notification}/>
-                )
-              }
-            }}
-          />
-          <Tab.Screen
-            name={navigationStrings.EDIT_PROFILE}
-            component={EditProfile}
-            options={{
-              tabBarIcon: ({focused}) =>{
-                return (
-                  <Image style={{tintColor: focused ? Colors.navyBlue : Colors.grey,width:40,height:40}} source={icons.edit}/>
-                )
-              }
-            }}
-          />
-        </Tab.Navigator>
-      );
-}
+const TabRoutes = ({ navigation }) => {
+  return (
+    <Tab.Navigator
+      screenOptions={{ headerShown: false, tabBarShowLabel: false }}
+    >
+      <Tab.Screen
+        name={navigationStrings.HOME_SCREEN}
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                style={{
+                  tintColor: focused ? Colors.navyBlue : Colors.grey,
+                  width: moderateScale(30),
+                  height: moderateScale(30),
+                }}
+                source={icons.home}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name={navigationStrings.NOTIFICATION}
+        component={Notification}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                style={{
+                  tintColor: focused ? Colors.navyBlue : Colors.grey,
+                  width: moderateScale(35),
+                  height: moderateScale(35),
+                }}
+                source={icons.notification}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name={navigationStrings.EDIT_PROFILE}
+        component={EditProfile}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <Image
+                style={{
+                  tintColor: focused ? Colors.navyBlue : Colors.grey,
+                  width: moderateScale(35),
+                  height: moderateScale(35),
+                }}
+                source={icons.edit}
+              />
+            );
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default TabRoutes;
