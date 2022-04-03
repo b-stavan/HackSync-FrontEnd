e13da00d26b20081216656786a0205d180cef6b0
Stavan Bhatt <stavan.b@simformsolutions.com>
2022-04-03 16:42:01 +0530

    Refactored UI

--- app/modules/EditProfile/EditProfile.js
+++ app/modules/EditProfile/EditProfile.js
@@ -1,56 +1,49 @@
-import { View, Text, Image, TouchableOpacity} from 'react-native'
-import React from 'react'
-import { styles } from './style/EditProfileStyle'
-import images from '../../assets/images'
-import icons from '../../assets/icons'
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import { styles } from "./style/EditProfileStyle";
+import images from "../../assets/images";
+import icons from "../../assets/icons";
 const EditProfile = () => {
   return (
     <View style={styles.mainContainer}>
       <View style={styles.content}>
-      <View style={styles.imageContainer}>
-        <Image source={icons.person} style={styles.image}/>
-        <View style={styles.nameContainer}>
-          <Text style={styles.nameText}>Heiden Biermann</Text>
-          <Text>Hello</Text>
-        </View>
-      </View>
-      <View style={styles.dataContainer}>
-        <View style={styles.cardMenu}>
-          <Image source={icons.person} style={styles.iconImage}/>
-          <View style={styles.textContainer}>
-            <Text style={styles.heading}>Login Details</Text>
-            <Text>Username, Password</Text>
+        <View style={styles.imageContainer}>
+          <Image source={icons.person} style={styles.image} />
+          <View style={styles.nameContainer}>
+            <Text style={styles.nameText}>Heiden Biermann</Text>
           </View>
         </View>
-        <View style={styles.cardMenu}>
-          <Image source={icons.help} style={styles.iconImage}/>
-          <View style={styles.textContainer}>
-            <Text style={styles.heading}>Login Details</Text>
-            <Text>Username, Password</Text>
+        <View style={styles.dataContainer}>
+          <View style={styles.cardMenu}>
+            <Image source={icons.person} style={styles.iconImage} />
+            <View style={styles.textContainer}>
+              <Text style={styles.heading}>Users Details</Text>
+
+              <Text>Username, Password</Text>
+            </View>
+          </View>
+          <View style={styles.cardMenu}>
+            <Image source={icons.help} style={styles.iconImage} />
+            <View style={styles.textContainer}>
+              <Text style={styles.heading}>FAQs</Text>
+              <Text>Username, Password</Text>
+            </View>
+          </View>
+          <View style={styles.cardMenu}>
+            <Image source={icons.details} style={styles.iconImage} />
+            <View style={styles.textContainer}>
+              <Text style={styles.heading}>Login Details</Text>
+              <Text>Username, Password</Text>
+            </View>
           </View>
         </View>
-        <View style={styles.cardMenu}>
-          <Image source={icons.details} style={styles.iconImage}/>
-          <View style={styles.textContainer}>
-            <Text style={styles.heading}>Login Details</Text>
-            <Text>Username, Password</Text>
-          </View>
-        </View>
-        <View style={styles.cardMenu}>
-          <Image source={icons.person} style={styles.iconImage}/>
-          <View style={styles.textContainer}>
-            <Text style={styles.heading}>Login Details</Text>
-            <Text>Username, Password</Text>
-          </View>
-        </View>
-      </View>   
         <TouchableOpacity style={styles.button}>
-          <Image source={icons.logOut} style={styles.iconImage}/>
+          <Image source={icons.logOut} style={styles.iconImage} />
           <Text style={styles.logOut}>Log Out</Text>
         </TouchableOpacity>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default EditProfile
+export default EditProfile;
--- app/modules/EditProfile/style/EditProfileStyle.js
+++ app/modules/EditProfile/style/EditProfileStyle.js
@@ -1,68 +1,71 @@
-import { StyleSheet} from 'react-native'
-import Colors from '../../../themes/Colors'
-import { horizontalScale, moderateScale, verticalScale } from '../../../themes/Metrices'
+import { StyleSheet } from "react-native";
+import Colors from "../../../themes/Colors";
+import {
+  horizontalScale,
+  moderateScale,
+  verticalScale,
+} from "../../../themes/Metrices";
 
 export const styles = StyleSheet.create({
-    mainContainer: {
-        flex:1,
-    },
-    content:{
-        margin:20,
-        flex:1,
-        justifyContent: 'space-between'
-    },
-    imageContainer: {
-        alignItems: 'center',
-        paddingVertical: verticalScale(20),
-    },
-    image: {
-        width: 150,
-        height: 150,
-        borderRadius: 30,
-    },
-    nameContainer:{
-        marginTop: verticalScale(20),
-        alignItems: 'center',
-    },
-    dataContainer: {
-        flex:1,
-    },
-    nameText: {
-        fontSize: 20,
-        fontWeight: '600',
-        color: Colors.black,
-    },
-    cardMenu: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        flex:1,
-        borderWidth: 1,
-    },
-    iconImage: {
-        width: 30,
-        height: 30,
-    },
-    textContainer: {
-        marginLeft: 20,
-        //borderWidth: 1,
-        justifyContent: 'space-around',
-        height: verticalScale(50)
-    },
-    heading: {
-        fontWeight: 'bold',
-        color: Colors.black,
-    },
-    button: {
-        borderWidth: 1,
-        borderColor: Colors.navyBlue,
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: verticalScale(40),
-        borderRadius: 20,
-        flexDirection: 'row',
-    },
-    logOut: {
-        fontWeight: 'bold',
-    }
-})
+  mainContainer: {
+    flex: 1,
+  },
+  content: {
+    margin: 20,
+    flex: 1,
+    justifyContent: "space-between",
+  },
+  imageContainer: {
+    alignItems: "center",
+    paddingVertical: verticalScale(20),
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 30,
+  },
+  nameContainer: {
+    marginTop: verticalScale(20),
+    alignItems: "center",
+  },
+  dataContainer: {
+    flex: 1,
+  },
+  nameText: {
+    fontSize: 20,
+    fontWeight: "600",
+    color: Colors.black,
+  },
+  cardMenu: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    marginTop: verticalScale(40),
+  },
+  iconImage: {
+    width: 30,
+    height: 30,
+  },
+  textContainer: {
+    marginLeft: 20,
+    justifyContent: "space-around",
+    height: verticalScale(50),
+  },
+  heading: {
+    fontWeight: "bold",
+    color: Colors.navyBlue,
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: Colors.navyBlue,
+    justifyContent: "center",
+    alignItems: "center",
+    height: verticalScale(40),
+    borderRadius: 10,
+    flexDirection: "row",
+    marginHorizontal: horizontalScale(40),
+  },
+  logOut: {
+    fontWeight: "bold",
+  },
+});
